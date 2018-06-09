a7dfb122ec02c244de1db6faf74de1fcaba13337
Mike Smith <mikerhyssmith@googlemail.com>
2018-06-09 19:38:37 +0100

    fix barreling from context menu bug

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,8 @@ import Barrelr from "./barrelr";
 export function activate(context: vscode.ExtensionContext) {
     const barrelr: Barrelr = new Barrelr();
 
-    const barrel = vscode.commands.registerCommand("extension.barrel", () => {
-        const args = [...arguments];
-        const filePath = args[0] && args[0].fsPath ? args[0].fsPath : path.dirname(vscode.window.activeTextEditor.document.fileName);
+    const barrel = vscode.commands.registerCommand("extension.barrel", (folder) => {
+        const filePath = folder && folder.fsPath ? folder.fsPath : path.dirname(vscode.window.activeTextEditor.document.fileName);
         barrelr.barrel(filePath)
         .catch(err => {
             vscode.window.showErrorMessage(err);
